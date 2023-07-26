17cecddbb84f643f5c1cd1325899ed3a280a9c1b
Shin ain <s22028@gsm.hs.kr>
2023-07-26 14:22:51 +0900

    Add TabBarVC

---
+++ ParkSeoJun/Sources/Presentation/TabBar/TabBarVC.swift
@@ -0,0 +1,50 @@
+//
+//  TabBarVC.swift
+//  ParkSeoJun
+//
+//  Created by 신아인 on 2023/07/26.
+//
+
+import UIKit
+
+class TabBarVC: UITabBarController, UITabBarControllerDelegate {
+    
+    let HEIGHT_TAB_BAR:CGFloat = 100
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+        
+        let homeVC = VC()
+        let writeVC = VC()
+        let mypageVC = VC()
+        
+        tabBar.tintColor = UIColor(rgb: 0x5074F3)
+        tabBar.backgroundColor = .white
+        
+        // set tabbar shadow
+        tabBar.layer.masksToBounds = false
+        tabBar.layer.shadowColor = UIColor.black.cgColor
+        tabBar.layer.shadowOpacity = 0.2
+        tabBar.layer.shadowOffset = CGSize(width: 0, height: 0)
+        tabBar.layer.shadowRadius = 6
+        
+        homeVC.tabBarItem = UITabBarItem(title: "홈", image: UIImage(systemName: "house")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(systemName: "house.fill")?.withRenderingMode(.alwaysOriginal))
+        writeVC.tabBarItem = UITabBarItem(title: "이벤트", image: UIImage(systemName: "square.and.pencil")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(systemName: "square.and.pencil")?.withRenderingMode(.alwaysOriginal))
+        mypageVC.tabBarItem = UITabBarItem(title: "마이", image: UIImage(systemName: "person.circle")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(systemName: "person.circle.fill")?.withRenderingMode(.alwaysOriginal))
+        
+        let navigationHome = UINavigationController(rootViewController: homeVC)
+        let navigationWrite = UINavigationController(rootViewController: writeVC)
+        let navigationMypage = UINavigationController(rootViewController: mypageVC)
+        
+        setViewControllers([navigationHome, navigationWrite, navigationMypage], animated: false)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        var tabFrame = self.tabBar.frame
+        tabFrame.size.height = HEIGHT_TAB_BAR
+        tabFrame.origin.y = self.view.frame.size.height - HEIGHT_TAB_BAR
+        self.tabBar.frame = tabFrame
+    }
+}
