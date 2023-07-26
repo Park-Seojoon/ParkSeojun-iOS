3905da9529e1d908b8172105c38c3204c22e2fb0
junseopark0331 <junseopark0331@icloud.com>
2023-07-26 11:47:33 +0900

    기본 파일 구조 및 라이브러리 생성

---
+++ ParkSeoJun/Sources/Utility/Base/BaseVC.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+class BaseVC: UIViewController {
+    // MARK: - Properties
+    let bounds = UIScreen().bounds
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupBackgroundIfNotSet()
+        addView()
+        setup()
+        setLayout()
+        bind()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setLayoutSubviews()
+    }
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        print("\(type(of: self)): \(#function)")
+    }
+    // MARK: - Method
+    private func setupBackgroundIfNotSet() {
+        if self.view.backgroundColor == nil {
+            self.view.backgroundColor = .white
+        }
+    }
+    
+    
+    func addView() {}
+    func setup() {}
+    func setLayout() {}
+    func setLayoutSubviews() {}
+    func bind() {}
+}
+
+
